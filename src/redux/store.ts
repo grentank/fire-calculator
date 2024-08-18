cd3746b6cca5fa4a00805424da8a716dfb4a0a88
grentank <mexmat100394@gmail.com>
2024-08-18 13:06:37 +0300

    removed errors

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import calculatorReducer from './slices/calculator';
-// import modalSlice from './slices/modal';
 
 export const store = configureStore({
   reducer: {
     calculator: calculatorReducer,
-    // modal: modalSlice,
   },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
